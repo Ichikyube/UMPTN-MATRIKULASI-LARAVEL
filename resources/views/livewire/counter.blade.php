d10da3c9054a3c25a3afa5fff9cab773aaca9b7c
Ichikyube <ichikyube@gmail.com>
2022-10-28 19:25:46 +0700

    lifewire installed

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- Care about people's approval and you will be their prisoner. --}}
+</div>
+<div style="text-align: center">
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+</div>
