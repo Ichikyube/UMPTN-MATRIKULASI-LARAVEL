7d57f175cfbc1ee128137dfbfd24e8f581fe0264
Ichikyube <ichikyube@gmail.com>
2022-10-28 16:36:47 +0700

    adding some models

---
+++ app/Http/Controllers/PainterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Model\Painter;
+use Illuminate\Http\Request;
+
+class PainterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Model\Painter  $painter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Painter $painter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Model\Painter  $painter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Painter $painter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Model\Painter  $painter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Painter $painter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Model\Painter  $painter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Painter $painter)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PaintingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Model\Painting;
+use Illuminate\Http\Request;
+
+class PaintingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Model\Painting  $painting
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Painting $painting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Model\Painting  $painting
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Painting $painting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Model\Painting  $painting
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Painting $painting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Model\Painting  $painting
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Painting $painting)
+    {
+        //
+    }
+}
